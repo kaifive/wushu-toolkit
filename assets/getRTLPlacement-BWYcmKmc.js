ecec2f8643e2eb4a05fb8c90e402ea0c07c0dbae
unknown <tuongkhai.nguyen@gmail.com>
2024-05-19 15:54:40 -0400

    Updates

--- assets/getRTLPlacement-BWYcmKmc.js
+++
@@ -1,1 +0,0 @@
-import{q as e}from"./DefaultLayout-DV5R5ybZ.js";var a=function(t,r){switch(t){case"right":return e(r)?"left":"right";case"left":return e(r)?"right":"left";default:return t}};export{a as g};
