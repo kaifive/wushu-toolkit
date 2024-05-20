37277fbb96e429a36da1edd1d7ff706e75ec45cc
unknown <tuongkhai.nguyen@gmail.com>
2024-05-20 13:11:14 -0400

    Updates

---
+++ assets/CAlertLink-N2ueWNsU.js
@@ -0,0 +1,1 @@
+import{r as n,_ as i,R as m,b as o,c as p,P as e}from"./index-BPEmNkZ8.js";import{C as d}from"./index.es-CM1h9AoC.js";var a=n.forwardRef(function(s,r){var l=s.children,t=s.className,c=i(s,["children","className"]);return m.createElement(d,o({className:p("alert-link",t)},c,{ref:r}),l)});a.propTypes={children:e.node,className:e.string};a.displayName="CAlertLink";export{a as C};
