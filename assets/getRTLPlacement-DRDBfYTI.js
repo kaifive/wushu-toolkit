a4e72f77dd6aaa8b79618318f713e4a874c016ee
unknown <tuongkhai.nguyen@gmail.com>
2024-05-20 22:41:22 -0400

    Updates

--- assets/getRTLPlacement-DRDBfYTI.js
+++
@@ -1,1 +0,0 @@
-import{q as e}from"./DefaultLayout-DYgob1Kc.js";var a=function(t,r){switch(t){case"right":return e(r)?"left":"right";case"left":return e(r)?"right":"left";default:return t}};export{a as g};
