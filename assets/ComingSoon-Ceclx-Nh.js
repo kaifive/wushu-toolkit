a8a371f67e9bc0c4d686485e359c420dabebf52a
unknown <tuongkhai.nguyen@gmail.com>
2025-05-21 10:29:51 -0400

    Updates

---
+++ assets/ComingSoon-Ceclx-Nh.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-C41FOwII.js";import{C as o,a as s}from"./CRow-BqMaQlsm.js";import{C as e}from"./CAlert-7K4ld3YY.js";import"./authService-D2lGObD4.js";const m=()=>r.jsx(r.Fragment,{children:r.jsx(o,{children:r.jsx(s,{xs:12,children:r.jsx(e,{color:"warning",children:"This feature is coming soon, please check back later."})})})});export{m as default};
--- assets/getRTLPlacement-BJfUXoqk.js
+++
@@ -1,1 +0,0 @@
-import{m as e}from"./DefaultLayout-chPr7Y7H.js";var a=function(t,r){switch(t){case"right":return e(r)?"left":"right";case"left":return e(r)?"right":"left";default:return t}};export{a as g};
